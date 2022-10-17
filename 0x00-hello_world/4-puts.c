4f28bdc65800978c377828422bab324426c0ffb9
omolejie destiny <finxzy4@gmail.com>
2022-10-17 13:32:37 -0700

    new ones

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
-/*
+/**
  * main - print a line using puts function
+ *
  * Return: 0
- */ 
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
