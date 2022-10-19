6f4a625ddb91aa92c5c3f594501cfd5e487df8bd
omolejie destiny <finxzy4@gmail.com>
2022-10-19 02:57:47 -0700

    newttyfhg

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print all lowercase alphbet
+ *
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	int letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		_putchar(letter);
+	}
+	_putchar('\n');
+}
